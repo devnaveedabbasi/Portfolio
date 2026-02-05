a81b2e09c76735e52076ef34ff1a425997e0faf3
Naveed Abbasi <naveedabbasi03111309060@gmail.com>
2026-02-05 21:22:17 +0500

    chatbot not working

--- component/layout.tsx
+++ component/layout.tsx
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 // Lazy load components
 const LazyAnimation = lazy(() => import("@/component/partials/animation"));
 const LazySetting = lazy(() => import("@/component/partials/setting"));
-const LazyChatBot = lazy(() => import("@/component/partials/chatbot/page"));
+// const LazyChatBot = lazy(() => import("@/component/partials/chatbot/page"));
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -32,9 +32,9 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         <Navigations />
       </div>
 
-      <Suspense fallback={null}>
+      {/* <Suspense fallback={null}>
         <LazyChatBot />
-      </Suspense>
+      </Suspense> */}
     </div>
   );
 };
